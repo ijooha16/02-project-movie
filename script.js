d5e5b963dc5f16410a12aa62522e251ed24d9f71
Juha Yoon <ijooha16@gmail.com>
2025-01-09 15:48:07 +0900

    key_export

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
+import config from "./etc/apiKey.js";
+
+const apiKey = config.apiKey;
+
 const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWIyZjliMmQ0OGRhMTA2NTE3MWUwMzU1OWQ3OGVkYyIsIm5iZiI6MTczNjM0MDI3Ny4yNDQsInN1YiI6IjY3N2U3MzM1NDRkNjQ5ZmZhZTdiMGI4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.toCBc4fQg0V2zfy7VZJqgSnQrqF35pPrYzy6cSuJTP8'
+      Authorization: apiKey,
     }
   };
 
-// fetch ('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error("Could not fetch resource");
-//         }
-//         return response.json();
-//     })
-//     .then(data => console.log(data))
-//     .catch(error => console.error(error));
+const topRated = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
 
-
-async function fetchData() {
+async function fetchData(url) {
     try {
 
-        const movieTitle = document.querySelector("search").value.toLowerCase();
-        const response = await fetch ('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options);
+        const response = await fetch (url, options);
 
         if (!response.ok) {
             throw new Error ("Could not fetch resource");
@@ -34,3 +28,5 @@ async function fetchData() {
         console.error(error);
     }
 }
+
+fetchData(topRated);
