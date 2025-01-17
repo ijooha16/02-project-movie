e2d03e4d31ddd199f43868924b2c3153dc908de4
Juha Yoon <ijooha16@gmail.com>
2025-01-17 20:26:25 +0900

    main

--- js/modalOpenClose.js
+++ js/modalOpenClose.js
@@ -5,9 +5,6 @@ function openModal(data, bookMark, movieListContent) {
     const modalContent = document.querySelector(".modal_content");
     const local = JSON.parse(localStorage.getItem('Bookmarked')) || [];
 
-    console.log(data)
-    
-
     const clickedCard = card.target.closest(".card");
     const movie = (await data).filter((movie) => Number(movie.id) === Number(clickedCard.id))[0];
 
@@ -31,7 +28,7 @@ function openModal(data, bookMark, movieListContent) {
     modalContainer.classList.remove("hide");
     document.body.style.overflow = "hidden";
 
-    if (local.some(data => data.title === movie.title)) { //데이터가 이미 있으면
+    if (await local.some(data => data.title === movie.title)) { //데이터가 이미 있으면
       bookMark.style.backgroundImage = "url('./src/icon_heart_empty.png')";
     } else { //새로운 데이터일 때
         bookMark.style.backgroundImage = "url('./src/icon_heart_fill.png')";
